f39084ce854b8ff1df1684f231946fcde4bee63d
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2023-08-07 07:15:28 +0530

    shayari-api

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+var cors = require('cors')
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3400;
+console.log(process.env.OPENAI_API_KEY)
+app.use(express.json());
+app.use(cors())
+app.get('/shayari', async (req, res) => {
+    const keyword = req.query.keyword;
+    console.log(keyword)
+    try {
+        const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+            prompt: `Shayari about ${keyword}`,
+            max_tokens: 100,
+            temperature: 0.7,
+            n: 1
+        }, {
+            headers: {
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const shayari = response.data.choices[0].text.trim();
+        res.json({ shayari });
+    } catch (error) {
+        console.error('Error:', error.response.data);
+        res.status(500).json({ error: 'Something went wrong' });
+    }
+});
+console.clear()
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
